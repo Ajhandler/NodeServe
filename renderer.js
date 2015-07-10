40c21c7d3bf02ef0338fb830f8c530107b655150
Alexander Handler <Alexjhandler@gmail.com>
2015-07-10 00:31:52 -0400

    templates can now populate dynamic content

--- renderer.js
+++ renderer.js
@@ -1,11 +1,20 @@
 var fs = require("fs");
 
+function mergeValues(values,content){
+	//cycle over keys
+	for(var key in values){
+		//replace all {{key}} with the vaules from the values object
+		content = content.replace("{{"+ key +"}}", values[key]);
+	}
+	return content
+}
+
 function view(templateName, values, response){
 	//read from the template files
-	var fc = fs.readFileSync("./views/"+ templateName + ".html");
+	var fc = fs.readFileSync("./views/"+ templateName + ".html", {encoding: "utf8"});
 
 	//insert values into content
-
+	fc = mergeValues(values,fc)
 	//write out to the response
   		response.write(fc);
 }
